c1405c8169f0df6b787f6973fe0cd6bf49f9ec4f
Alan Bi <alan.bi326@gmail.com>
2020-03-25 21:46:59 -0700

    v1.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="detecto",
-    version="1.1.2",
+    version="1.1.3",
     author="Alan Bi",
     author_email="alan.bi326@gmail.com",
     description="Build fully-functioning computer vision models with 5 lines of code",
