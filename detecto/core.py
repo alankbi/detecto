7bff23dbed71219ad10959fa15380e22ba4fd0ca
Alan Bi <alan.bi326@gmail.com>
2021-08-22 17:15:09 -0400

    Add file and small TODO

--- detecto/core.py
+++ detecto/core.py
@@ -249,7 +249,8 @@ class Model:
             Valid choices are ``fasterrcnn_resnet50_fpn``,
             ``fasterrcnn_mobilenet_v3_large_fpn``, and
             ``fasterrcnn_mobilenet_v3_large_320_fpn``. Defaults to
-            ``fasterrcnn_resnet50_fpn``.
+            ``fasterrcnn_resnet50_fpn``. (Note: this parameter will be made
+            available in the next release)
         :type model_name: str
 
         **Example**::
@@ -262,7 +263,7 @@ class Model:
         self._device = device if device else config['default_device']
 
         # Load a model pre-trained on COCO
-        if model_name == self.DEFAULT:
+        if model_name == self.DEFAULT:  # TODO: update docs when released
             self._model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=pretrained)
         elif model_name == self.MOBILENET:
             self._model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=pretrained)
