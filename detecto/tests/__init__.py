5005cc82ba35c2b3644c288a991a9efdd83364b8
1nF0rmed <prkinformed@gmail.com>
2021-08-26 23:12:29 +0530

    Adds method to download model file
    The `test_model_predict` test requires that a model file `model.pth` exists for it to pass.
    This adds a method to download that file before running the tests to ensure that during a new setup, tests don't fail.

--- detecto/tests/__init__.py
+++ detecto/tests/__init__.py
@@ -1,1 +1,27 @@
 from .helpers import get_image
+from requests import get
+import logging
+import os
+
+MODEL_URL = "https://www.dropbox.com/s/kjalrfs3la97du8/model.pth?dl=1"
+LOGGER = logging.getLogger(__name__)
+
+
+def _download_model_file():
+    path = os.path.dirname(__file__)
+    model_path = os.path.join(path, 'static/model.pth')
+
+    if os.path.isfile(model_path):
+        LOGGER.info("Model file already exists. Continuing.")
+        return
+
+    LOGGER.info("\nDownloading model file......")
+
+    with open(model_path, "wb") as model_file:
+        response = get(MODEL_URL)
+        model_file.write(response.content)
+
+    LOGGER.info("Done")
+
+
+_download_model_file()
